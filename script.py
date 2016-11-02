921997780dd44f6fc81e04fd8dde77202a86e00f
muqsith <muqsithirfan@gmail.com>
2016-11-02 22:05:38 +0530

    Added requirements.txt ($ pip freeze > requirements )

--- script.py
+++ script.py
@@ -8,7 +8,9 @@ from mod_xml2csv import xml2csv
 from mod_csv2mysql import load_csvs
 
 parser = argparse.ArgumentParser(description='7z -> XML -> csv -> MySQL')
-parser.add_argument("file_path",help="Please pass the following as argument \n7z file path (OR) directory path where xmls are located")
+parser.add_argument("file_path",help='''Please pass the following as argument
+	7z file path (OR) directory path where xmls are located
+	''')
 args = parser.parse_args()
 
 csvs_dir = ''
